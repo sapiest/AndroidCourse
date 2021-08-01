4a8519511f5181adb2e521258b6e1d2d9980eca5
Anton Mikhaylenko <toxaxb27@gmail.com>
2021-08-01 23:11:21 +1000

    Add task 5

---
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -0,0 +1,253 @@
+package ru.skillbranch.skillarticles.markdown
+
+import java.util.regex.Pattern
+
+object MarkdownParser {
+
+    private val LINE_SEPARATOR = System.getProperty("line.separator") ?: "\n"
+
+    //group regex
+    private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+$)"
+    private const val ORDERED_LIST_ITEM_GROUP = "(^\\d. .+$)"
+    private const val HEADER_GROUP = "(^#{1,6} .+?$)"
+    private const val QUOTE_GROUP = "(^> .+?$)"
+    private const val ITALIC_GROUP = "((?<!\\*)\\*[^*].*?[^*]?\\*(?!\\*)|(?<!_)_[^_].*?[^_]?_(?!_))"
+    private const val BOLD_GROUP = "((?<!\\*)\\*{2}[^*].*?[^*]?\\*{2}(?!\\*)|(?<!\\_)\\_{2}[^_].*?[^_]?\\_{2}(?!\\_))"
+    private const val STRIKE_GROUP = "((?<!\\~)\\~{2}[^~].*?[^~]?\\~{2}(?!\\~))"
+    private const val RULE_GROUP = "(^[-_*]{3}$)"
+    private const val INLINE_GROUP = "((?<!`)`[^`\\s].*?[^`\\s]?`(?!`))"
+    private const val LINK_GROUP = "(\\[[^\\[\\]]*?]\\(.+?\\)|^\\[*?]\\(.*?\\))"
+    private const val BLOCK_CODE_GROUP = ""
+
+
+    //result regex
+    const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP|" +
+            "$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP|" +
+            "$ORDERED_LIST_ITEM_GROUP|$BLOCK_CODE_GROUP"
+
+    private val elementsPattern by lazy { Pattern.compile(MARKDOWN_GROUPS, Pattern.MULTILINE) }
+
+    /**
+     * parse markdown text to elements
+     */
+
+    fun parse(string: String): MarkdownText {
+        val elements = mutableListOf<Element>()
+        elements.addAll(findElements(string))
+        return MarkdownText(elements)
+    }
+
+    /**
+     * clear markdown text to string without markdown characters
+     */
+
+    fun clear(string: String): String? {
+        return null
+    }
+
+    /**
+     *  find markdown elements in markdown text
+     */
+
+    private fun findElements(string: CharSequence): List<Element> {
+        val parents = mutableListOf<Element>()
+        val matcher = elementsPattern.matcher(string)
+        var lastStartIndex = 0
+
+        loop@ while (matcher.find(lastStartIndex)) {
+            val startIndex = matcher.start()
+            val endIndex = matcher.end()
+
+            if (lastStartIndex < startIndex) {
+                parents.add(Element.Text(string.subSequence(lastStartIndex, startIndex)))
+            }
+
+            var text: CharSequence
+
+            val groups = 1..10
+            var group = -1
+            for (gr in groups) {
+                if (matcher.group(gr) != null) {
+                    group = gr
+                    break
+                }
+            }
+
+            when (group) {
+                -1 -> break@loop
+
+                // UNORDERED_GROUP
+                1 -> {
+                    text = string.subSequence(startIndex.plus(2), endIndex)
+
+                    val subs = findElements(text)
+                    val element = Element.UnorderedListItem(text, subs)
+                    parents.add(element)
+
+                    lastStartIndex = endIndex
+                }
+
+                //HEADERS
+                2 -> {
+                    val reg = "^#{1,6}".toRegex().find(string.subSequence(startIndex, endIndex))
+                    val level = reg!!.value.length
+
+                    text = string.subSequence(startIndex.plus(level.inc()), endIndex)
+
+                    val element = Element.Header(level, text)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //QUOTE
+                3 -> {
+                    text = string.subSequence(startIndex.plus(2), endIndex)
+                    val subElements = findElements(text)
+                    val element = Element.Quote(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //INTALIC
+                4 -> {
+                    text = string.subSequence(startIndex.inc(), endIndex.dec())
+                    val subElements = findElements(text)
+                    val element = Element.Italic(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //BOLD
+                5 -> {
+                    text = string.subSequence(startIndex.plus(2), endIndex.minus(2))
+                    val subElements = findElements(text)
+                    val element = Element.Bold(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //STRIKE
+                6 -> {
+                    text = string.subSequence(startIndex.plus(2), endIndex.minus(2))
+                    val subElements = findElements(text)
+                    val element = Element.Strike(text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //RULE
+                7 -> {
+                    val element = Element.Rule()
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //CODE
+                8 -> {
+                    text = string.subSequence(startIndex.inc(), endIndex.dec())
+                    val element = Element.InlineCode(text)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //LINK
+                9 -> {
+                    text = string.subSequence(startIndex, endIndex)
+                    val (title: String, link: String) = "\\[(.*)]\\((.*)\\)".toRegex().find(text)!!.destructured
+                    val element = Element.Link(link, title)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                //ORDERED_LINK
+                10 -> {
+                    val reg = "^\\d.".toRegex().find(string.subSequence(startIndex, endIndex))
+                    val valueLength = reg!!.value.length
+                    val order = reg.value
+                    text = string.subSequence(startIndex.plus(valueLength.inc()), endIndex)
+                    val subElements = findElements(text)
+                    val element = Element.OrderedListItem(order, text, subElements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+            }
+        }
+
+        if (lastStartIndex < string.length) {
+            val text = string.subSequence(lastStartIndex, string.length)
+            parents.add(Element.Text(text))
+        }
+
+        return parents
+    }
+}
+
+data class MarkdownText(val elements: List<Element>)
+
+sealed class Element {
+    abstract val text: CharSequence
+    abstract val elements: List<Element>
+
+    data class Text(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class UnorderedListItem(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Header(
+        val level: Int = 1,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Quote(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Italic(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Bold(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Strike(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Rule(
+        override val text: CharSequence = "",
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class InlineCode(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Link(
+        val link: String,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class OrderedListItem(
+        val order: String,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class BlockCode(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+}
--- app/src/test/java/ru/skillbranch/skillarticles/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/skillarticles/ExampleUnitTest.kt
@@ -1,195 +1,258 @@
-//package ru.skillbranch.skillarticles
-//
-//import org.junit.Assert.*
-//
-///**
-// * Example local unit test, which will execute on the development machine (host).
-// *
-// * See [testing documentation](http://d.android.com/tools/testing).
-// */
-//
-//import org.junit.After
-//import org.junit.Assert
-//import org.junit.Test
-//
-///**
-// * Example local unit test, which will execute on the development machine (host).
-// *
-// * See [testing documentation](http://d.android.com/tools/testing).
-// */
-//class ExampleUnitTest {
-//
-//    /**
-//    Добавьте метод в UserHolder для очистки значений UserHolder после выполнения каждого теста,
-//    это необходимо чтобы тесты можно было запускать одновременно
-//
-//    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
-//    fun clearHolder(){
-//    map.clear()
-//    }
-//     */
-//    @After
-//    fun after(){
-//        UserHolder.clearHolder()
-//    }
-//
-//    @Test
-//    fun register_user_success() {
-//        val holder = UserHolder
-//        val user = holder.registerUser("John Doe", "John_Doe@unknown.com","testPass")
-//        val expectedInfo = """
-//            firstName: John
-//            lastName: Doe
-//            login: john_doe@unknown.com
-//            fullName: John Doe
-//            initials: J D
-//            email: John_Doe@unknown.com
-//            phone: null
-//            meta: {auth=password}
-//        """.trimIndent()
-//
-//        Assert.assertEquals(expectedInfo, user.userInfo)
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun register_user_fail_blank() {
-//        val holder = UserHolder
-//        holder.registerUser("", "John_Doe@unknown.com","testPass")
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun register_user_fail_illegal_name() {
-//        val holder = UserHolder
-//        holder.registerUser("John Jr Doe", "John_Doe@unknown.com","testPass")
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun register_user_fail_illegal_exist() {
-//        val holder = UserHolder
-//        holder.registerUser("John Doe", "John_Doe@unknown.com","testPass")
-//        holder.registerUser("John Doe", "John_Doe@unknown.com","testPass")
-//    }
-//
-//    @Test
-//    fun register_user_by_phone_success() {
-//        val holder = UserHolder
-//        val user = holder.registerUserByPhone("John Doe", "+7 (917) 971 11-11")
-//        val expectedInfo = """
-//            firstName: John
-//            lastName: Doe
-//            login: +79179711111
-//            fullName: John Doe
-//            initials: J D
-//            email: null
-//            phone: +79179711111
-//            meta: {auth=sms}
-//        """.trimIndent()
-//
-//        Assert.assertEquals(expectedInfo, user.userInfo)
-//        Assert.assertNotNull(user.accessCode)
-//        Assert.assertEquals(6, user.accessCode?.length)
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun register_user_by_phone_fail_blank() {
-//        val holder = UserHolder
-//        holder.registerUserByPhone("", "+7 (917) 971 11-11")
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun register_user_by_phone_fail_illegal_name() {
-//        val holder = UserHolder
-//        holder.registerUserByPhone("John Doe", "+7 (XXX) XX XX-XX")
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun register_user_failby_phone_illegal_exist() {
-//        val holder = UserHolder
-//        holder.registerUserByPhone("John Doe", "+7 (917) 971-11-11")
-//        holder.registerUserByPhone("John Doe", "+7 (917) 971-11-11")
-//    }
+package ru.skillbranch.skillarticles
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+import org.junit.Assert
+import org.junit.Test
+import ru.skillbranch.skillarticles.markdown.Element
+import ru.skillbranch.skillarticles.markdown.MarkdownParser
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+class UnitTest {
+    @Test
+    fun parse_list_item() {
+        val result = MarkdownParser.parse(unorderedListString)
+        val actual = prepare<Element.UnorderedListItem>(result.elements)
+        Assert.assertEquals(expectedUnorderedList, actual)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_ordered_list_item() {
+        val result = MarkdownParser.parse(orderedListString)
+        val actualOrderedList = prepare<Element.OrderedListItem>(result.elements)
+        val actualLevels = result.elements.spread()
+            .filterIsInstance<Element.OrderedListItem>()
+            .map { it.order }
+        Assert.assertEquals(listOf("1.", "2."), actualLevels)
+        Assert.assertEquals(expectedOrderedList, actualOrderedList)
+    }
+
+
+
+    @Test
+    fun parse_header() {
+        val result = MarkdownParser.parse(headerString)
+        val actual = prepare<Element.Header>(result.elements)
+        val actualLevels = result.elements.spread()
+            .filterIsInstance<Element.Header>()
+            .map{it.level}
+        Assert.assertEquals(expectedHeader, actual)
+        Assert.assertEquals(listOf(1, 2, 3, 4, 5, 6), actualLevels)
+    }
+
+    @Test
+    fun parse_quote() {
+        val result = MarkdownParser.parse(quoteString)
+        val actual = prepare<Element.Quote>(result.elements)
+        Assert.assertEquals(expectedQuote, actual)
+    }
+
+    @Test
+    fun parse_italic() {
+        val result = MarkdownParser.parse(italicString)
+        val actual = prepare<Element.Italic>(result.elements)
+        Assert.assertEquals(expectedItalic, actual)
+    }
+
+    @Test
+    fun parse_bold() {
+        val result = MarkdownParser.parse(boldString)
+        val actual = prepare<Element.Bold>(result.elements)
+        Assert.assertEquals(expectedBold, actual)
+    }
+
+    @Test
+    fun parse_strike() {
+        val result = MarkdownParser.parse(strikeString)
+        val actual = prepare<Element.Strike>(result.elements)
+        Assert.assertEquals(expectedStrike, actual)
+    }
+
+    @Test
+    fun parse_combine() {
+        val result = MarkdownParser.parse(combineEmphasisString)
+        val actualItalic= prepare<Element.Italic>(result.elements)
+        val actualBold= prepare<Element.Bold>(result.elements)
+        val actualStrike= prepare<Element.Strike>(result.elements)
+        Assert.assertEquals(expectedCombine["italic"], actualItalic)
+        Assert.assertEquals(expectedCombine["bold"], actualBold)
+        Assert.assertEquals(expectedCombine["strike"], actualStrike)
+    }
+
+    @Test
+    fun parse_rule() {
+        val result = MarkdownParser.parse(ruleString)
+        val actual = prepare<Element.Rule>(result.elements)
+        Assert.assertEquals(3, actual.size)
+    }
+
+    @Test
+    fun parse_inline_code() {
+        val result = MarkdownParser.parse(inlineString)
+        val actual = prepare<Element.InlineCode>(result.elements)
+        Assert.assertEquals(expectedInline, actual)
+    }
 //
 //    @Test
-//    fun login_user_success() {
-//        val holder = UserHolder
-//        holder.registerUser("John Doe", "John_Doe@unknown.com","testPass")
-//        val expectedInfo = """
-//            firstName: John
-//            lastName: Doe
-//            login: john_doe@unknown.com
-//            fullName: John Doe
-//            initials: J D
-//            email: John_Doe@unknown.com
-//            phone: null
-//            meta: {auth=password}
-//        """.trimIndent()
-//
-//        val successResult =  holder.loginUser("john_doe@unknown.com", "testPass")
-//
-//        Assert.assertEquals(expectedInfo, successResult)
+//    fun parse_multiline_code() {
+//        val result = MarkdownParser.parse(multilineCode)
+//        val actual = prepare<Element.BlockCode>(result.elements) //optionally
+//        Assert.assertEquals(expectedMultilineCode, actual) //optionally
 //    }
+//
+    @Test
+    fun parse_link() {
+        val result = MarkdownParser.parse(linkString)
+        val actual = prepare<Element.Link>(result.elements)
+        val actualLink = result.elements.spread()
+            .filterIsInstance<Element.Link>()
+            .map{it.link}
+
+        Assert.assertEquals(expectedLink["titles"], actual)
+        Assert.assertEquals(expectedLink["links"], actualLink)
+    }
 //
 //    @Test
-//    fun login_user_by_phone_success() {
-//        val holder = UserHolder
-//        val user = holder.registerUserByPhone("John Doe", "+7 (917) 971-11-11")
-//        val expectedInfo = """
-//            firstName: John
-//            lastName: Doe
-//            login: +79179711111
-//            fullName: John Doe
-//            initials: J D
-//            email: null
-//            phone: +79179711111
-//            meta: {auth=sms}
-//        """.trimIndent()
+//    fun parse_images() {
+//        val result = MarkdownParser.parse(imagesString)
+//        val actual = prepare<Element.Image>(result.elements)
+//        val actualLink = result.elements.spread()
+//            .filterIsInstance<Element.Image>()
+//            .map{it.url}
 //
-//        val successResult =  holder.loginUser("+7 (917) 971-11-11", user.accessCode!!)
+//        val actualAlts = result.elements.spread()
+//            .filterIsInstance<Element.Image>()
+//            .map{it.alt}
 //
-//        Assert.assertEquals(expectedInfo, successResult)
+//        Assert.assertEquals(expectedImages["titles"], actual)
+//        Assert.assertEquals(expectedImages["alts"], actualAlts)
+//        Assert.assertEquals(expectedImages["links"], actualLink)
 //    }
+//
+    @Test
+    fun parse_all() {
+        val result = MarkdownParser.parse(markdownString)
+        val actualUnorderedList = prepare<Element.UnorderedListItem>(result.elements)
+        val actualHeaders = prepare<Element.Header>(result.elements)
+        val actualQuotes = prepare<Element.Quote>(result.elements)
+        val actualItalic = prepare<Element.Italic>(result.elements)
+        val actualBold = prepare<Element.Bold>(result.elements)
+        val actualStrike = prepare<Element.Strike>(result.elements)
+        val actualRule = prepare<Element.Rule>(result.elements)
+        val actualInline = prepare<Element.InlineCode>(result.elements)
+        val actualLinkTitles = prepare<Element.Link>(result.elements)
+        val actualLinks = result.elements.spread()
+            .filterIsInstance<Element.Link>()
+            .map { it.link }
+
+        Assert.assertEquals(expectedMarkdown["unorderedList"], actualUnorderedList)
+        Assert.assertEquals(expectedMarkdown["header"], actualHeaders)
+        Assert.assertEquals(expectedMarkdown["quote"], actualQuotes)
+        Assert.assertEquals(expectedMarkdown["italic"], actualItalic)
+        Assert.assertEquals(expectedMarkdown["bold"], actualBold)
+        Assert.assertEquals(expectedMarkdown["strike"], actualStrike)
+        Assert.assertEquals(3, actualRule.size)
+        Assert.assertEquals(expectedMarkdown["inline"], actualInline)
+        Assert.assertEquals(expectedMarkdown["linkTitles"], actualLinkTitles)
+        Assert.assertEquals(expectedMarkdown["links"], actualLinks)
+    }
 //
 //    @Test
-//    fun login_user_fail() {
-//        val holder = UserHolder
-//        holder.registerUser("John Doe", "John_Doe@unknown.com","testPass")
-//
-//        val failResult =  holder.loginUser("john_doe@unknown.com", "test")
-//
-//        Assert.assertNull(failResult)
+//    fun clear_all() {
+//        val result = MarkdownParser.clear(markdownString)
+//        Assert.assertEquals(markdownClearString, result)
 //    }
+//
 //
 //    @Test
-//    fun login_user_not_found() {
-//        val holder = UserHolder
-//        holder.registerUser("John Doe", "John_Doe@unknown.com","testPass")
-//
-//        val failResult =  holder.loginUser("john_cena@unknown.com", "test")
-//
-//        Assert.assertNull(failResult)
+//    fun clear_all_with_optionally() {
+//        val result = MarkdownParser.clear(markdownString)
+//        Assert.assertEquals(markdownOptionallyClearString, result)
 //    }
 //
+//    //optionally (delete @Ignore fo run)
 //    @Test
-//    fun request_access_code() {
-//        val holder = UserHolder
-//        val user = holder.registerUserByPhone("John Doe", "+7 (917) 971-11-11")
-//        val oldAccess = user.accessCode
-//        holder.requestAccessCode("+7 (917) 971-11-11")
+//    fun parse_all_with_optionally() {
+//        val result = MarkdownParser.parse(markdownString)
+//        val actualUnorderedList = prepare<Element.UnorderedListItem>(result.elements)
+//        val actualHeaders = prepare<Element.Header>(result.elements)
+//        val actualQuotes = prepare<Element.Quote>(result.elements)
+//        val actualItalic = prepare<Element.Italic>(result.elements)
+//        val actualBold = prepare<Element.Bold>(result.elements)
+//        val actualStrike = prepare<Element.Strike>(result.elements)
+//        val actualRule = prepare<Element.Rule>(result.elements)
+//        val actualInline = prepare<Element.InlineCode>(result.elements)
+//        val actualLinkTitles = prepare<Element.Link>(result.elements)
+//        val actualLinks = result.elements.spread()
+//            .filterIsInstance<Element.Link>()
+//            .map { it.link }
+//        val actualBlockCode = prepare<Element.BlockCode>(result.elements) //optionally
+//        val actualOrderedList = prepare<Element.OrderedListItem>(result.elements) //optionally
 //
-//        val expectedInfo = """
-//            firstName: John
-//            lastName: Doe
-//            login: +79179711111
-//            fullName: John Doe
-//            initials: J D
-//            email: null
-//            phone: +79179711111
-//            meta: {auth=sms}
-//        """.trimIndent()
-//
-//        val successResult =  holder.loginUser("+7 (917) 971-11-11", user.accessCode!!)
-//
-//        Assert.assertNotEquals(oldAccess, user.accessCode!!)
-//        Assert.assertEquals(expectedInfo, successResult)
+//        Assert.assertEquals(expectedMarkdown["unorderedList"], actualUnorderedList)
+//        Assert.assertEquals(expectedMarkdown["header"], actualHeaders)
+//        Assert.assertEquals(expectedMarkdown["quote"], actualQuotes)
+//        Assert.assertEquals(expectedMarkdown["italic"], actualItalic)
+//        Assert.assertEquals(expectedMarkdown["bold"], actualBold)
+//        Assert.assertEquals(expectedMarkdown["strike"], actualStrike)
+//        Assert.assertEquals(3, actualRule.size)
+//        Assert.assertEquals(expectedMarkdown["inline"], actualInline)
+//        Assert.assertEquals(expectedMarkdown["linkTitles"], actualLinkTitles)
+//        Assert.assertEquals(expectedMarkdown["links"], actualLinks)
+//        Assert.assertEquals(expectedMarkdown["multiline"], actualBlockCode)
+//        Assert.assertEquals(expectedMarkdown["orderedList"], actualOrderedList)
 //    }
-//}
+//
+//
+//
+    private fun printResults(list:List<String>){
+        val iterator = list.iterator()
+        while (iterator.hasNext()){
+            println("find >> ${iterator.next()}")
+        }
+    }
+
+    private fun printElements(list:List<Element>){
+        val iterator = list.iterator()
+        while (iterator.hasNext()){
+            println("element >> ${iterator.next()}")
+        }
+    }
+
+    private fun Element.spread():List<Element>{
+        val elements = mutableListOf<Element>()
+        elements.add(this)
+        elements.addAll(this.elements.spread())
+        return elements
+    }
+
+    private fun List<Element>.spread():List<Element>{
+        val elements = mutableListOf<Element>()
+        if(this.isNotEmpty()) elements.addAll(
+            this.fold(mutableListOf()){acc, el -> acc.also { it.addAll(el.spread()) }}
+        )
+        return elements
+    }
+
+    private inline fun <reified T:Element> prepare(list:List<Element>) : List<String>{
+        return list
+            .fold(mutableListOf<Element>()){ acc, el -> //spread inner elements
+                acc.also { it.addAll(el.spread()) }
+            }
+            .filterIsInstance<T>() //filter only expected instance
+            .map { it.text.toString() } //transform to element text
+    }
+}
