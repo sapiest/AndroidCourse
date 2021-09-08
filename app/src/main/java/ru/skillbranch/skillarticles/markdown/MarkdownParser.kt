29bb7466bf53ec336313c5da93bc52fbf3ef512f
Anton Mikhaylenko <toxaxb27@gmail.com>
2021-09-08 18:27:14 +0300

    Update task 5 clear

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -47,21 +47,45 @@ object MarkdownParser {
 
     fun clear(string: String): String? {
         val result = parse(string)
-        val str = StringBuilder()
-//        result.elements.forEach {
-//            str.append(clearChildren(it))
-//        }
-        return str.toString()
+        val builder = StringBuilder()
+        result.elements.forEach {
+            clearChildren(it, builder)
+        }
+        return builder.toString()
     }
 
-    private fun clearChildren(element: Element): String{
-        val str = StringBuilder()
-        str.append(element.text)
-//        element.elements.forEach {
-//            str.append(clearChildren(it))
-//        }
-        return str.toString()
+    private fun clearChildren(element: Element, builder: StringBuilder): CharSequence {
+        return builder.apply {
+            when (element) {
+                is Element.Text -> append(element.text)
+
+                is Element.UnorderedListItem -> {
+                    for (child in element.elements) {
+                        clearChildren(child, builder)
+                    }
+                }
+
+                is Element.Quote -> {
+                    for (child in element.elements) {
+                        clearChildren(child, builder)
+                    }
+                }
+
+                is Element.Header -> append(element.text)
+
+                is Element.Italic, is Element.Bold, is Element.Strike -> {
+                    for (child in element.elements) {
+                        clearChildren(child, builder)
+                    }
+                }
+
+                is Element.Rule, is Element.InlineCode, is Element.Link -> append(element.text)
+
+                else -> append(element.text)
+            }
+        }
     }
+
 
     /**
      *  find markdown elements in markdown text
